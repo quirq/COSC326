c229bbc7eeceb5d5b7c8efd641c96ec777fc321c
Zade <v4g2b@ugrad.cs.ubc.ca>
2019-02-28 10:44:35 +1300

    Removed tovalidate.txt

--- task7/Task7.java
+++ task7/Task7.java
@@ -1,9 +1,34 @@
 package task7;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Task7{
 
-	public static void main(String[] args){
-		
+	public Task7(){
+		validate(readText());
 	}
+
+	private void validate(ArrayList<String> emails){
+		for(int i = 0; i < emails.size(); i++){
+
+		}
+	}
+
+	private ArrayList readText(){
+		Scanner scanner = new Scanner(System.in);
+		ArrayList emails = new ArrayList<String>();
+
+		while(scanner.hasNext()){
+			emails.add(scanner.next());
+		}
+
+		return emails;
+	}
+
+	public static void main(String[] args){
+		new Task7();
+	}
+
 
 }
