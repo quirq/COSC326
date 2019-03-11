bcefd6c5a43113c11158412f1699e64fa4b886af
sean <seanl.12090@gmail.com>
2019-03-12 12:25:03 +1300

    Added iterator

--- task6/Task6.java
+++ task6/Task6.java
@@ -4,10 +4,14 @@ import java.util.Scanner;
 
 public class Task6 {
     public static void main(String[] args) {
+        int base;
+        int size;
+        int iterator;
+
         Scanner scan = new Scanner(System.in);
 
         System.out.println("What base are you adding in?");
-        int base = Integer.parseInt(scan.nextLine());
+        base = Integer.parseInt(scan.nextLine());
         System.out.println("What is the first number to be added?");
         String first = scan.nextLine();
         System.out.println("What is the second number to be added?");
@@ -16,7 +20,7 @@ public class Task6 {
         String[] firstString = first.split("(?!^)");
         String[] secondString = second.split("(?!^)");
 
-        int size = firstString.length;
+        size = firstString.length;
         if(secondString.length > firstString.length) {
             size = secondString.length;
         }
@@ -25,16 +29,20 @@ public class Task6 {
         for(int i = 0; i < firstString.length - size; i++) {
             firstNumber[i] = 0;
         }
+        iterator = 0;
         for(int i = firstString.length - size; i < firstString.length; i++) {
-            firstNumber[i] = Integer.parseInt(firstString[i]);
+            firstNumber[i] = Integer.parseInt(firstString[iterator]);
+            iterator++;
         }
 
         int[] secondNumber = new int[size];
         for(int i = 0; i < secondString.length - size; i++) {
             secondNumber[i] = 0;
         }
+        iterator = 0;
         for(int i = secondString.length - size; i < secondString.length; i++) {
-            secondNumber[i] = Integer.parseInt(secondString[i]);
+            secondNumber[i] = Integer.parseInt(secondString[iterator]);
+            iterator++;
         }
 
     }
